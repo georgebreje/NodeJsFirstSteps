230c736eb2ce399602d952933c8b2d5e45518cef
George Breje <george.breje2@gmail.com>
2023-04-19 14:42:30 +0300

    Add CRUD with MongoDB

--- NodeJsExpressMongo/controllers/authController.js
+++ NodeJsExpressMongo/controllers/authController.js
@@ -1,8 +1,4 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fsPromises = require('fs').promises;
@@ -12,7 +8,7 @@ const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required' });
     
-    const foundUser = usersDB.users.find(u => u.username === user);
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401);
 
     const match = await bcrypt.compare(pwd, foundUser.password);
@@ -36,14 +32,10 @@ const handleLogin = async (req, res) => {
         );
 
         // Saving refreshToken with current user
-        const otherUsers = usersDB.users.filter(u => u.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log (result);
+        
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
         res.json({ accessToken });
