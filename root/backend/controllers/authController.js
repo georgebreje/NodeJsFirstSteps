a4aed5d56a5b7d22a943a95c3821c801c16dff02
George Breje <george.breje2@gmail.com>
2023-04-22 17:41:26 +0300

    Add Refresh Token Rotation and Reuse Detection
    Use String array refreshToken for multiple devices access
    
    Reuse Detection
     - check for reusability of the same refresh token
      - not found token means that it has been already invalidated
       - invalidation process is below this check where the array from the db for the foundUser is updated with a new array that contains the new refresh token
       - when reuse detected clear the refreshToken array for the hackedUser
       - err states that the token expired which will result a status 403
    
    Refresh Token Rotation
     - for a foundUser remove the old refreshToken and add a new refreshToken
    
    Delete the refreshToken on logout

--- root/backend/controllers/authController.js
+++ root/backend/controllers/authController.js
@@ -5,9 +5,10 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 const handleLogin = async (req, res) => {
+    const cookies = req.cookies;
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required' });
-    
+
     const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401);
 
@@ -15,27 +16,34 @@ const handleLogin = async (req, res) => {
     if (match) {
         const roles = Object.values(foundUser.roles);
         const accessToken = jwt.sign(
-            { 
+            {
                 "UserInfo": {
                     "username": foundUser.username,
-                    "roles": roles 
+                    "roles": roles
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
 
-        const refreshToken = jwt.sign(
+        const newRefreshToken = jwt.sign(
             { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
 
+        const newRefreshTokenArray =
+            !cookies?.jwt
+                ? foundUser.refreshToken
+                : foundUser.refreshToken.filter(rt => rt !== cookies.jwt);
+
+        if (cookies.jwt) res.clearCookie('jwt', { httpOnly: true, sameSite: 'None' });
+
         // Saving refreshToken with current user
-        foundUser.refreshToken = refreshToken;
+        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
         const result = await foundUser.save();
-        console.log (result);
-        
+        console.log(result);
+
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
         res.json({ accessToken });
