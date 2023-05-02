3b353dbe781ade24adc8599853c7956a8031ea67
George Breje <george.breje2@gmail.com>
2023-05-02 15:37:18 +0300

    Add SSG to users with generateStaticParams
    Static Site Generation
    While on a specific user page add generateStaticParams which will keep all the requests from the previous users page without making new ones

--- root/frontend/src/my-app/app/users/[userId]/page.tsx
+++ root/frontend/src/my-app/app/users/[userId]/page.tsx
@@ -3,6 +3,7 @@ import getUserPosts from "@/lib/getUserPosts";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 import { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
 
 type Params = {
     params: {
@@ -36,4 +37,13 @@ export default async function UserPage({ params: { userId } }: Params) {
             </Suspense>
         </>
     )
+}
+
+export async function generateStaticParams() {
+    const usersData: Promise<User[]> = getAllUsers();
+    const users = await usersData;
+
+    return users.map(user => ({
+        userId: user.id.toString()
+    }))
 }
